523304ab82fbb4890f4639932267d1bd7e7ea108
KaiserHua1901 <133273705+KaiserHua1901@users.noreply.github.com>
2024-12-12 13:50:33 -0800

    Add Physic and collision

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,1 +1,13 @@
 extends CharacterBody2D
+
+class_name player 
+
+const GRAVITY:float = 690.0 
+
+
+
+
+func _physics_process(delta: float) -> void:
+    if is_on_floor() == false:
+        velocity.y += GRAVITY * delta
+    move_and_slide()
