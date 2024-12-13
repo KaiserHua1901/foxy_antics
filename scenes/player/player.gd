179535c6cb4dc59c89bf50b9fc57ea7f5786ceac
KaiserHua1901 <133273705+KaiserHua1901@users.noreply.github.com>
2024-12-13 11:04:08 -0800

    Stable Build

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -7,28 +7,35 @@ const RUN_SPEED:float = 120.0
 const MAX_FALL:float = 600.0 
 const JUMP_VELOCITY:float = -360.0 
 
-@onready var Sprite:Sprite2D = $Sprite2D
+@onready var sprite:Sprite2D = $Sprite2D
+@onready var debug_label:Label = $DebugLabel
+
 
 func _physics_process(delta: float) -> void:
     if is_on_floor() == false:
         velocity.y += GRAVITY * delta
     get_input()
     move_and_slide()
+    updated_debug_label()
     # print(velocity.y)
 
 func get_input() -> void:
     #stop the movement when not pressed anything
     velocity.x = 0
+
     # move left 
     if Input.is_action_pressed("left") == true:
         velocity.x = -RUN_SPEED 
-        Sprite.flip_h = true
+        sprite.flip_h = true
+
     #move right
     elif Input.is_action_pressed("right") == true:
         velocity.x = RUN_SPEED
-        Sprite.flip_h = false
+        sprite.flip_h = false
 
     if Input.is_action_just_pressed("jump") == true and is_on_floor() == true:
         velocity.y = JUMP_VELOCITY
     velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
 
+func updated_debug_label() -> void:
+    debug_label.text = "floor:%s\n%.0f, %.0f" % [is_on_floor(), velocity.x, velocity.y ]
