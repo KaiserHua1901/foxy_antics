8f28a59b3ec2a723646452e62ae9a5581e5868d5
KaiserHua1901 <133273705+KaiserHua1901@users.noreply.github.com>
2024-12-13 11:34:05 -0800

    set state

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
 class_name player 
+enum PlayerState {IDLE, RUN, JUMP, FALL, HURT}
+
+
 
 const GRAVITY:float = 690.0 
 const RUN_SPEED:float = 120.0 
@@ -9,13 +12,16 @@ const JUMP_VELOCITY:float = -360.0
 
 @onready var sprite:Sprite2D = $Sprite2D
 @onready var debug_label:Label = $DebugLabel
+@onready var animation:AnimationPlayer = $AniPlayer 
 
+var _state:PlayerState = PlayerState.IDLE
 
 func _physics_process(delta: float) -> void:
     if is_on_floor() == false:
         velocity.y += GRAVITY * delta
     get_input()
     move_and_slide()
+    caculate_state()
     updated_debug_label()
     # print(velocity.y)
 
@@ -38,4 +44,30 @@ func get_input() -> void:
     velocity.y = clampf(velocity.y, JUMP_VELOCITY, MAX_FALL)
 
 func updated_debug_label() -> void:
-    debug_label.text = "floor:%s\n%.0f, %.0f" % [is_on_floor(), velocity.x, velocity.y ]
+    debug_label.text = "floor:%s\n velocity:x(%.0f), y(%.0f) \n state:%s" % [is_on_floor(), velocity.x, velocity.y, PlayerState.keys()[_state] ]
+
+func set_state(new_state:PlayerState):
+    if new_state == _state:
+        return 
+    _state = new_state
+    match _state:
+        PlayerState.IDLE:
+            animation.play("idle")
+        PlayerState.RUN:
+            animation.play("run")
+        PlayerState.FALL:
+            animation.play("falling")
+        PlayerState.JUMP:
+            animation.play("jump")
+
+func caculate_state(): 
+    if is_on_floor() == true:
+        if velocity.x == 0:
+            set_state(PlayerState.IDLE)
+        else: 
+            set_state(PlayerState.RUN)
+    else:
+        if velocity.y < 0:
+            set_state(PlayerState.JUMP)
+        else: 
+            set_state(PlayerState.FALL)
